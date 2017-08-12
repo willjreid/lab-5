096b37fb881f70a4c71e7f2d627dc9b14723edec
William J Reid <willjreid@gmail.com>
2017-08-11 17:34:31 -0700

    submitting question 4

--- app.js
+++ app.js
@@ -68,11 +68,13 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var add = sum(sum(testArray[0], testArray[1])[0], testArray[2])[0];
+  var say = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + add + ' is their sum.';
+  return [add, say];
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
